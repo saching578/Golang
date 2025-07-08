0119539e447e1d3136681b2c73bf7a446ac629b6
Sachin Gupta <sachingupta578@gmail.com>
2025-07-08 11:54:20 +0530

    2D arrays declaration

--- arrays/main.go
+++ arrays/main.go
@@ -35,4 +35,13 @@ func main() {
 
 	fmt.Println(arr4)
 
+	//2D array
+	var arr5 [3][3]int = [3][3]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+
+	for i := 0; i < len(arr5); i++ {
+		for j := 0; j < len(arr5[i]); j++ {
+			fmt.Println(arr5[i][j])
+		}
+	}
+
 }
