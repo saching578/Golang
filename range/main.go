77267abb95f6f2a243a62fb2b726ae0afce4e44f
Sachin Gupta <sachingupta578@gmail.com>
2026-05-23 19:57:24 +0530

    range

---
+++ range/main.go
@@ -0,0 +1,31 @@
+package main
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+
+	//iterate over the slice using range
+	for _, n := range nums {
+		println(n)
+	}
+
+	m := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+	//iterate over the map using range
+	for k, v := range m {
+		println(k, v)
+	}
+}
+
+
+//output:
+1
+2
+3
+4
+5
+a 1
+b 2
+c 3
